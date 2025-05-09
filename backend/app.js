3e00376f3054e040a8838dc72e34af168fdfb8c2
JEYASURIYAK <jeyasuriya3545@gmail.com>
2025-05-09 20:30:55 +0530

    Files Updated

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+
+
+
+
+app.listen(8000, () => {
+    console.log('Server is listening PORT 8000 in Development');
+})
